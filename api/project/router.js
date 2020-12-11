e7e22db837daee28b848fcb0a402d15b1a96adb4
will Fletcher <wfletch7@gmail.com>
2020-12-11 11:44:18 -0500

    trying to fix api/project POST

--- api/project/router.js
+++ api/project/router.js
@@ -3,8 +3,18 @@ const express = require('express')
 const Project = require('./model')
 const router = express.Router()
 
+
+// const forNpmTest = (req, res, next) => {
+//     console.log(req.body.name)
+//     if(!req.body.name){
+//         res.error(400).json({message:'i hate npm test'})
+//     }else{
+//         next()
+//     }
+// }
+
+
 router.get('/', (req, res) => {
-    console.log(req.body)
     Project.getAll()
     .then(proj => {
         res.status(200).json(proj)
@@ -12,6 +22,7 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({message:err.message}))
 })
 router.post('/', (req, res) => {
+    console.log(req.body.name)
     Project.create(req.body)
     .then(proj => {
         if(proj){
